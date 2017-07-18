6f524a0c3b33c4f294905e5fecc95fad2d984aad
Jawagnercarena <julianwagner@live.com>
2017-07-18 15:11:22 -0500

    Edited the lar_visualize file to take any file and produce downsize images that give a much nicer and clearer view of the event

--- lar_visualize.py
+++ lar_visualize.py
@@ -17,6 +17,8 @@ to look at the dsets.)
 import pylab
 import sys
 import h5py
+import numpy as np
+import matplotlib.pyplot as plt
 
 max_evts = 10
 evt_plotted = 0
@@ -72,7 +74,45 @@ except KeyError:
 
 f.close()
 
+def my_range(start, end, step):
+    while start <= end:
+        yield start
+        start += step
+
+shp = list(data.shape)
+shp[-1] = shp[-1] / 4   # downsample by 4
+simpledata=np.zeros(shp)
+
+for event_n in range(data.shape[0]):
+    y=0
+    for x in my_range(0, 4096, 4):
+        if x == 0:
+            print 'checking shapes for event =', event_n
+            print '  simpledata slice shape =', simpledata[event_n,:,:,y:(y+1)].shape
+            print '  mean(data) slice shape =', np.nanmean(data[event_n,:,:,x:(x+4)], axis=2).shape
+        simpledata[event_n,:,:,y:(y+1)] = np.nanmean(data[event_n,:,:,x:(x+4)], axis=2).reshape(2,240,1)
+        y+=1
+
 colorbar_tile = 'scaled energy'
+
+def plot_event(img, minv= -500, maxv=1000):
+    fig = plt.figure(figsize=(12, 8))
+    gs = plt.GridSpec(1, 2)
+    for i in range(2):
+        ax = plt.subplot(gs[i])
+        ax.axis('on')
+        ax.xaxis.set_major_locator(plt.NullLocator())
+        ax.yaxis.set_major_locator(plt.NullLocator())
+
+        cmap = 'inferno'
+        im = ax.imshow(img[i], cmap=pylab.get_cmap(cmap),
+                       interpolation='nearest', vmin=minv, vmax=maxv)
+        cbar = plt.colorbar(im, fraction=0.04)
+        cbar.set_label(colorbar_tile, size=9)
+        cbar.ax.tick_params(labelsize=6)
+        plt.title("lar", fontsize=12)
+        plt.xlabel("wire", fontsize=10)
+        plt.ylabel("tick", fontsize=10)
 
 for counter, label in enumerate(labels):
     if evt_plotted > max_evts:
@@ -80,25 +120,8 @@ for counter, label in enumerate(labels):
     targ = labels[counter]
     pstring = '{}'.format(targ)
     print(pstring)
-    fig = pylab.figure(figsize=(12, 8))
-    gs = pylab.GridSpec(1, 2)
-    for i in range(2):
-        ax = pylab.subplot(gs[i])
-        ax.axis('on')
-        ax.xaxis.set_major_locator(pylab.NullLocator())
-        ax.yaxis.set_major_locator(pylab.NullLocator())
 
-        minv = -500
-        cmap = 'inferno'
-        im = ax.imshow(data[counter][i], cmap=pylab.get_cmap(cmap),
-                       interpolation='nearest', vmin=minv, vmax=3000)
-        #pylab.show()
-        cbar = pylab.colorbar(im, fraction=0.04)
-        cbar.set_label(colorbar_tile, size=9)
-        cbar.ax.tick_params(labelsize=6)
-        pylab.title("lar", fontsize=12)
-        pylab.xlabel("wire", fontsize=10)
-        pylab.ylabel("tick", fontsize=10)
+    plot_event(simpledata[counter])
 
     figname = 'evt_%s_%d_%f.pdf' % \
         (filename, counter, labels[counter])
